b585b9a16fb669619d7eb51fbc22083084121a36
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-18 14:12:38 +0200

    Configurado Vitest

--- intro.ts
+++ intro.ts
@@ -1,5 +1,14 @@
-function add(x: number, y: number): number {
-	return x + y;
+function add(...numbers: number[]): number {
+	let sum = 0;
+	for (let i = 0; i < numbers.length; i++) {
+		sum += numbers[i];
+	}
+	return sum;
 }
 
-console.log(add(3, 1));
+const x = [1, 2, 6];
+const y = [...x];
+
+console.log(x, y);
+
+console.log(add(1, 5, 6));
